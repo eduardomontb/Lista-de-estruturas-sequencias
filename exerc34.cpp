fc27170dfda6df7ce57638b4815879a7a6e85091
eduardomontb <eduardomontb@gmail.com>
2025-08-27 16:12:21 -0400

    Update exerc34.cpp

--- exerc34.cpp
+++ exerc34.cpp
@@ -1,3 +1,5 @@
+//Cada degrau de uma escada tem uma altura X. Faça um algoritmo que receba essa altura e a altura que o usuário deseja alcançar subindo a escada. Calcule e mostre quantos degraus o usuário deverá
+//subir para atingir seu objetivo.
 #include <stdio.h>
 #include <math.h>
 
