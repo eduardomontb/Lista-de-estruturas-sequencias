6e9500f185d283eb78f55e242331d695488f7344
eduardomontb <eduardomontb@gmail.com>
2025-08-27 16:24:18 -0400

    Update exerc38.cpp

--- exerc38.cpp
+++ exerc38.cpp
@@ -5,18 +5,24 @@
 
 int main(){
 	
+	//Declaração de variáveis
 	float tempo_gasto, velocidade_media, distancia, combustivel_gasto;
 	
+	//Entrada de dados
 	printf("Digite o tempo gasto da viagem:\n");
 	scanf("%f", &tempo_gasto);
 	
 	printf("Digite a velocidade media durante a viagem:\n");
 	scanf("%f", &velocidade_media);
 	
+	//Processamento de cálculos
+	//Calcula a distância percorrida na viagem
 	distancia = tempo_gasto * velocidade_media;
 	
+	//Calcula a quantidade de combustível gasto
 	combustivel_gasto = distancia / 12;
 	
+	//Saída de dados
 	printf("\n\nA distancia perdcorrida nessa viagem foi de %.2f metros\n", distancia);
 	
 	printf("A quantidade de combustível gasto nessa viagem foi %.2f litros", combustivel_gasto);
