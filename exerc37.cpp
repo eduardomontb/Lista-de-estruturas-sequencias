ab2604e62f93e231299890d06446992017337066
eduardomontb <eduardomontb@gmail.com>
2025-08-27 16:25:49 -0400

    Update exerc37.cpp

--- exerc37.cpp
+++ exerc37.cpp
@@ -7,19 +7,25 @@
 
 int main(){
 	
+	//Declaração de variáveis
 	float custo, preco_convite;
 	int min_convite, lucro23;
 	
+	//Entrada de dados
 	printf("Digite o valor do custo do espetaculo teatral realizado:\n");
 	scanf("%f", &custo);
 	
 	printf("Digite o valor do convite para esse espetaculo:\n");
 	scanf("%f", &preco_convite);
 	
+	//Processamento de cálculos
+	//Calcula a quantidade mínima de convites para cobrir o custo do espetáculo
 	min_convite = ceil(custo / preco_convite);
-	
+
+	//Calcula a quantidade de convites para obter lucro de 23%
 	lucro23 =  ceil((custo * 1.23) / preco_convite);
 	
+	//Saída de dados
 	printf("\n\nA quantidade de convites que devem ser vendidos para que pelo menos o custo do espetáculo seja alcançado e:\n %i", min_convite);
 	
 	printf("\n\nA quantidade de convites que devem ser vendidos para que se tenha um lucro de 23 porcento e:\n %i", lucro23);
