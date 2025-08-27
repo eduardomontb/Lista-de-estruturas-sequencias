6ef558d65b1c7c7381c0752420532455a96c4b08
eduardomontb <eduardomontb@gmail.com>
2025-08-27 16:29:14 -0400

    Update exerc33.cpp

--- exerc33.cpp
+++ exerc33.cpp
@@ -5,19 +5,25 @@
 
 int main(){
 	
+	//Declaração de variáveis
 	int ano_nascimento, ano_atual;
 	int  idade, idade2028;
 	
+	//Entrada de dados
 	printf("Digito o ano de nascimento:\n");
 	scanf("%d", &ano_nascimento);
 	
 	printf("Digite o ano atual:\n");
 	scanf("%d", &ano_atual);
 	
-	idade = 2025 - ano_nascimento;
+	//Processamento de cálculos
+	//Calcula a idade da pessoa
+	idade = ano_atual - ano_nascimento;
 	
+	//calcula a idade da pessoa em 2028
 	idade2028 = 2028 - ano_nascimento;
 	
+	//Saída de dados
 	printf("\n\nA pessoa tem %d anos de idade.", idade);
 	
 	printf("\n\nEm 2028 a pessoa tera %d anos de idade.", idade2028);
