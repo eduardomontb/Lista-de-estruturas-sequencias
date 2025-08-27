6745c36130402cb45226f52f42c6d55cc7682169
eduardomontb <eduardomontb@gmail.com>
2025-08-27 15:48:53 -0400

    Update exerc10.cpp

--- exerc10.cpp
+++ exerc10.cpp
@@ -1,9 +1,11 @@
+//Fa√ßa um algoritmo que calcule e imprima a √°rea das seguintes figuras geom√©tricas:
+//‚Ä¢ tri√¢ngulo; quadrado; c√≠rculo; trap√©zio; ret√¢ngulo; losango.
 #include <stdio.h>
 #include <math.h>
 
 int main(){
 	
-	//DeclaraÁ„o de vari·veis
+	//Declara√ß√£o de vari√°veis
 	int base_triangulo, altura_triangulo, area_triangulo;
 	int lado_quadrado, area_quadrado;
 	double raio_circulo, area_circulo;
@@ -15,37 +17,37 @@ int main(){
 	printf("Digite o valor da base do triangulo:\n");
 	scanf("%d", &base_triangulo);
 	
-	// -- TRI¬NGULO --
+	// -- TRI√ÇNGULO --
 	printf("Digite o valor da altura do triangulo:\n", altura_triangulo);
 	scanf("%d", &altura_triangulo);
 	
-	// FÛrmula da ·rea do tri‚ngulo
+	// F√≥rmula da √°rea do tri√¢ngulo
 	area_triangulo = (base_triangulo * altura_triangulo) / 2;
 	
-	//Mostra o resultado do calculo da ·rea do tri‚ngulo no monitor 
+	//Mostra o resultado do calculo da √°rea do tri√¢ngulo no monitor 
 	printf("A area do triangulo e:\n %d\n\n\n", area_triangulo);
 	
 	// -- QUADRADO --
 	printf("Digite o valor do lado do quadrado:\n");
 	scanf("%d", &lado_quadrado);
 	
-	// FÛrmula da ·rea do quadrado
+	// F√≥rmula da √°rea do quadrado
 	area_quadrado = lado_quadrado * lado_quadrado;
 	
-	//Mostra o resultado do c·lculo da ·rea do quadrado no monitor
+	//Mostra o resultado do c√°lculo da √°rea do quadrado no monitor
 	printf("A area do quadrado e:\n %d\n\n\n", area_quadrado);
 	
-	// -- CÕRCULO --
+	// -- C√çRCULO --
 	printf("Digite o valor do raio do circulo:\n");
 	scanf("%lf", &raio_circulo);
 	
-	//FÛrmula da ·rea do cÌrculo
+	//F√≥rmula da √°rea do c√≠rculo
 	area_circulo =  3.14 * raio_circulo * raio_circulo;
 	
-	//mostra o resultado do c·lculo da ·rea do quadrado no monitor
+	//mostra o resultado do c√°lculo da √°rea do quadrado no monitor
 	printf("A area do circulo e:\n %.2lf\n\n\n", area_circulo);
 	
-	// -- TRAP…ZIO -- 
+	// -- TRAP√âZIO -- 
 	printf("Digite o valor da primeira base do trapezio:\n");
 	scanf("%lf", &base1_trapezio);
 	
@@ -56,10 +58,10 @@ int main(){
 	printf("Digite o valor da altura do trapezio:\n");
 	scanf("%lf", &altura_trapezio);
 	
-	//FÛrmula da ·rea do trapÈzio
+	//F√≥rmula da √°rea do trap√©zio
 	area_trapezio = (base1_trapezio + base2_trapezio) * altura_trapezio / 2;
 	
-	//Mostra o resultado do c·lculo da ·rea do trapÈzio no monitor
+	//Mostra o resultado do c√°lculo da √°rea do trap√©zio no monitor
 	printf("A area do trapezio e: %.2lf\n\n\n", area_trapezio);
 	
 	// -- RETANGULO --
@@ -69,10 +71,10 @@ int main(){
 	printf("digite a altura do retangulo:\n");
 	scanf("%lf", &altura_retangulo);
 	
-	//FÛrmula da ·rea do retangulo
+	//F√≥rmula da √°rea do retangulo
 	area_retangulo = base_retangulo * altura_retangulo;
 	
-	//Mostra o resultado do c·lculo da ·rea do retangulo no monitor
+	//Mostra o resultado do c√°lculo da √°rea do retangulo no monitor
 	printf("A area do retangulo e:\n %.2lf\n\n\n", area_retangulo);
 	
 	// -- LOSANGO --
@@ -82,10 +84,10 @@ int main(){
 	printf("Digite o valor da diagonal menor do losango:\n");
 	scanf("%lf", &menor_diagonal);
 	
-	//FÛrmula da ·rea do retangulo
+	//F√≥rmula da √°rea do retangulo
 	area_losango = maior_diagonal * menor_diagonal / 2;
 	
-	//Mostra o resultado do c·lculo da ·rea do losango no monitor
+	//Mostra o resultado do c√°lculo da √°rea do losango no monitor
 	printf("A area do losango e:\n %.2lf\n\n\n", area_losango);
 	
 	return 0;
