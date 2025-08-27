91eceaa9e77116c73d4c3a5172dc013acaf8d678
eduardomontb <eduardomontb@gmail.com>
2025-08-27 16:29:46 -0400

    Update exerc32.cpp

--- exerc32.cpp
+++ exerc32.cpp
@@ -3,13 +3,18 @@
 
 int main(){
 	
+	//Declaração de variáveis
 	float antigo_salario, novo_salario;
 	
+	//Entrada de dados
 	printf("Digite o valor do salario atual:\n");
 	scanf("%f", &antigo_salario);
 	
+	//Processamento de cálculo
+	//Calcula o novo salário com acréscimo de 37%
 	novo_salario = antigo_salario + (antigo_salario * 37 / 100);
 	
+	//Saída de dados
 	printf("\n\nValor do novo salario com acrescimo de 38%:\n R$ %.2f", novo_salario);
 	
 	return 0;
