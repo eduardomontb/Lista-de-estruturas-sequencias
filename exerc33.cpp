1839a5e7ea1b20ee157ba7974c8c74f80d0ae5c6
eduardomontb <eduardomontb@gmail.com>
2025-08-27 16:11:57 -0400

    Update exerc33.cpp

--- exerc33.cpp
+++ exerc33.cpp
@@ -1,3 +1,6 @@
+//Faça um algoritmo que receba o ano de nascimento de uma pessoa e o ano atual, calcule e mostre:
+//• A idade dessa pessoa;
+//•Quantos anos ela terá em 2028.
 #include <stdio.h>
 
 int main(){
